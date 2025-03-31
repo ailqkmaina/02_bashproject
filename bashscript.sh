e2c2b77c5c93951b80d56223cdf5b8fdd1ff87af
SpasM <spas.matev@rondevops745.onmicrosoft.com>
2025-03-31 17:19:22 +0200

    Excercise 4 - User processes sorted

--- bashscript.sh
+++ bashscript.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+#Expanding script to read user input and sort by it.
 
-ps aux | grep `whoami`
+echo "Would you like to sort the user processes by memory or cpu?
+To sort by  memory enter 1
+To sort by  CPU enter 2"
+read sortkey
+
+if [ $sortkey = 1 ]
+then
+	ps aux | head -1; ps aux --sort=-%mem | grep -i `whoami`
+elif [ $sortkey = 2 ]
+then 
+	ps aux | head -1; ps aux --sort=-%cpu | grep -i `whoami`
+else
+	echo "Invalid input."
+fi
+
