1d85b7e7565dbe6712770c23e530fbfe14ad24f3
SpasM <spas.matev@rondevops745.onmicrosoft.com>
2025-03-31 17:31:58 +0200

    Exercise 5 - Number of user processes sorted

--- bashscript.sh
+++ bashscript.sh
@@ -6,13 +6,15 @@ echo "Would you like to sort the user processes by memory or cpu?
 To sort by  memory enter 1
 To sort by  CPU enter 2"
 read sortkey
+echo "How many processes do you want to see?"
+read limiter
 
 if [ $sortkey = 1 ]
 then
-	ps aux | head -1; ps aux --sort=-%mem | grep -i `whoami`
+	ps aux | head -1; ps aux --sort=-%mem | grep -i `whoami` | head -n "$limiter"
 elif [ $sortkey = 2 ]
 then 
-	ps aux | head -1; ps aux --sort=-%cpu | grep -i `whoami`
+	ps aux | head -1; ps aux --sort=-%cpu | grep -i `whoami` | head -n "$limiter"
 else
 	echo "Invalid input."
 fi
